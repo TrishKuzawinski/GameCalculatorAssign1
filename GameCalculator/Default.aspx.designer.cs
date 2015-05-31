08898c437ea8e1e17cc3b778935ef948484d66ec
TrishKuzawinski <patriciakuzawinski@gmail.com>
2015-05-31 13:40:14 -0400

    commit

--- GameCalculator/Default.aspx.designer.cs
+++ GameCalculator/Default.aspx.designer.cs
@@ -13,24 +13,6 @@ namespace GameCalculator {
     public partial class _Default {
         
         /// <summary>
-        /// rbWin control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.RadioButton rbWin;
-        
-        /// <summary>
-        /// rbLoss control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.RadioButton rbLoss;
-        
-        /// <summary>
         /// txtScore1 control.
         /// </summary>
         /// <remarks>
